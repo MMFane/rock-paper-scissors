7b37b16e056d020c095fcb0d062151e3e849fc2d
Sam Yeager <samantha.yeager@thespurgroup.com>
2022-01-13 21:03:19 -0800

    reorganize and define CPU tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import "./game/game-play.ts";
+import "./css/App.css";
+import "./view/player-comp";
+import "./model/game-play";
+import ComputerPlayerView from "./view/player-comp";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <p>Rock, Paper, Scissors</p>
+            <div id="play-area">
+                <ComputerPlayerView />
+                <ComputerPlayerView />
+            </div>
         </div>
     );
 }
---
+++ src/view/player-comp.tsx
@@ -0,0 +1,4 @@
+export default function ComputerPlayerView () {
+    return <p>ComputerPlayer</p>
+}
+
